9b622231e83acbc5f3f6309cdf01b4b4302d475f
Mikey <michael.williams@enspiral.com>
2017-06-05 01:02:09 +1200

    update font-awesome.js to be proper module
    code shouldn't be run outside `create` function

--- app/styles/css/font-awesome.js
+++ app/styles/css/font-awesome.js
@@ -4,17 +4,15 @@ const { join, dirname } = require('path')
 const { assign } = Object
 const styleResolve = require('style-resolve')
 
-const fontCssPath = styleResolve.sync('font-awesome')
-const css = fs.readFileSync(fontCssPath, 'utf8')
-  .replace(/\.{2}/g, (match) => join(dirname(fontCssPath), match))
-
-// TODO: for patchlite, may have to convert font urls into url(data:base64: ....) format
-
 exports.gives = nest('styles.css')
 
 exports.create = function (api) {
+  const fontCssPath = styleResolve.sync('font-awesome')
+  const css = fs.readFileSync(fontCssPath, 'utf8')
+    .replace(/\.{2}/g, (match) => join(dirname(fontCssPath), match))
+
+  // TODO: for patchlite, may have to convert font urls into url(data:base64: ....) format
   return nest('styles.css', (sofar = {}) => {
     return assign(sofar, { fontAwesome: css })
   })
 }
-
